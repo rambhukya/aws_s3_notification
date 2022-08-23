a93ce871cfefec7e889ef9c0785a3977504e27e1
Bhukya Ram Kumar <86021692+rambhukya@users.noreply.github.com>
2022-08-23 23:50:37 +0530

    Update s3.py

--- s3.py
+++ s3.py
@@ -7,7 +7,7 @@ s3 = boto3.client(
     's3',
     aws_access_key_id="",
     aws_secret_access_key="",
-    #aws_session_token="FwoGZXIvYXdzEML//////////wEaDIorM30+m8cy3rh+oSLFAdLZVh6hCcTnq0BJrlJWUIPRB1w/Hxmh3iGzehlbMLzIxroNYedxHR/WFNBi4V+nuIaiextpotUG2YXP59y4dmZlLbGqpuKqMMDiUdPul3PO+R20BiKu2XS5TMKo0zOv+V70TVhHzi59ZLDh2GbuOAGXtBNJ/y09mhElSK9kW3sdYITya3kO1vp9D4vRuLuqD8DbuNeMb9LEcKT0XHR4EVQuxYjWTJ+EteWEc/bmrdLWKpsZ9pW+lMVKazC6N1MuQpk9XctFKKKR/ZcGMi0XcOWpa2fubVuIl0d//nizc2Og2hCl9IJv3u2j4BweoB0Ae6pPmvTRJsdrz/0="
+    #aws_session_token=""
 )
 BUCKET_NAME='test9959'
 
